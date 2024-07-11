71e478c3989058679c0a62b39cc700049844e60b
Downdate <danielzareimain@gmail.com>
2024-07-11 14:26:06 +0330

    first commit

---
+++ dataTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace dataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 23;
+            long bigNumber = 9000000000;
+            
+
+            
+        }
+    }
+
+}
---
+++ test1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 23;
+
+            Console.WriteLine($"age : {age}");
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("GG");
+        }
+    }
+
+}
