6d98fcc0a23fe4c14723c3523598c26cf1feb640
Downdate <danielzareimain@gmail.com>
2024-07-12 13:07:47 +0330

    const added

---
+++ const/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace dataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int age = 23;
+            Console.WriteLine(age);
+
+            //can not change age constant
+
+            age = 24;
+
+            
+        }
+    }
+
+}
