b8f3a5b948c11013cb7ae49f93fc803b1bb0395c
Downdate <danielzareimain@gmail.com>
2024-07-12 14:32:43 +0330

    for loop compelete & some fixes

---
+++ forLoop/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace forLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("please enter a value: ");
+            int loopCounter = Convert.ToInt32(Console.ReadLine());
+
+            if (loopCounter <= 0){
+                Console.WriteLine("Sorry, please enter a value above 0");
+            }
+
+            else{
+                for (int i = 0; i < loopCounter; i++){
+                System.Console.WriteLine("hello");
+                }
+            }
+            
+            /*for (int i = 0; i < loopCounter ; i +=2) {
+
+                System.Console.WriteLine(i);
+
+            }*/
+            
+        }
+    }
+
+}
