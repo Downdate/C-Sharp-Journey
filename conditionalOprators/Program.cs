6c29a233e2a731bce6a3d8860fa348870d6e2d11
Downdate <danielzareimain@gmail.com>
2024-07-17 14:00:36 +0330

    finished conditional operators

---
+++ conditionalOprators/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace conditionalOprators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 10;
+
+            string result = age >= 0 ? "Valid" : "Invalid";
+            System.Console.WriteLine(result);
+            
+        }
+    }
+
+}
